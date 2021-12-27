35dcc18be6209f73b7daf58b22eb352fba860446
reasley <reasley.com@gmail.com>
2021-12-28 07:02:53 +0900

    Feat: 노래 재생 코드 추가
    디스코드 봇으로 노래를 재생할 수 있도록 작성
    - !play 명령어

---
+++ src/functions/music.js
@@ -0,0 +1,25 @@
+const ytdl = require("ytdl-core-discord")
+const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource } = require('@discordjs/voice');
+
+export async function play(msg) {
+    const args = msg.content.split(" ")
+    const songInfo = await ytdl.getInfo(args[1]);
+    const song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url,
+    };
+
+    const connection = joinVoiceChannel({
+        channelId: msg.member.voice.channelId,
+        guildId: msg.guild.id,
+        adapterCreator: msg.guild.voiceAdapterCreator
+    });
+
+    const stream = await ytdl(song.url, { filter: 'audioonly', quality: 'highestaudio', format: 'mp3', hightWaterMark: 1<<25, type: 'opus' });
+    const player = createAudioPlayer()
+    var resource = createAudioResource(stream)
+    player.play(resource)
+    connection.subscribe(player);
+
+    await msg.reply(`:arrow_forward:  Now Playing *** ${song.title} ***`)
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] })
-const config = require("./config.json");
+const { Client, Intents } = require('discord.js')
+
+const sleep = require("./functions/sleep.js")
+const music = require("./functions/music.js")
+const { prefix, token } = require("./config.json")
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] })
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.user.setActivity('개발', { type: 'WATCHING' })
+})
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+client.on('messageCreate', async msg => {
+    // Prefix & Bot message check
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return
 
-client.login(config.token);
+
+    // Play
+    if (msg.content.startsWith(`${prefix}play`)) {
+        music.play(msg)
+    }
+})
+
+client.login(token)
