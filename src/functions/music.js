497e91536e212e6d8d1173dd9794cc5d2526161c
reasley <reasley.com@gmail.com>
2021-12-28 17:48:15 +0900

    Chore: 예외처리 추가
    오류 처리
    - 잘못된 링크일 경우 예외처리

--- src/functions/music.js
+++ src/functions/music.js
@@ -3,28 +3,29 @@ const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioRe
 
 export async function play(msg) {
     const args = msg.content.split(" ")
+
     try {
         const songInfo = await ytdl.getInfo(args[1]);
         const song = {
             title: songInfo.videoDetails.title,
             url: songInfo.videoDetails.video_url,
         };
-    } catch(err) {
-        return msg.reply(`:arrow_forward: Wrong link`)
-    }
+
+        const connection = joinVoiceChannel({
+            channelId: msg.member.voice.channelId,
+            guildId: msg.guild.id,
+            adapterCreator: msg.guild.voiceAdapterCreator
+        });
     
+        const stream = await ytdl(song.url, { filter: 'audioonly', quality: 'lowest', format: 'mp3', hightWaterMark: 1<<25 });
+        const player = createAudioPlayer()
+        var resource = createAudioResource(stream)
+        player.play(resource)
+        connection.subscribe(player);
+    
+        await msg.reply(`:arrow_forward:  Now Playing *** ${song.title} ***`)
 
-    const connection = joinVoiceChannel({
-        channelId: msg.member.voice.channelId,
-        guildId: msg.guild.id,
-        adapterCreator: msg.guild.voiceAdapterCreator
-    });
-
-    const stream = await ytdl(song.url, { filter: 'audioonly', quality: 'highestaudio', format: 'mp3', hightWaterMark: 1<<25, type: 'opus' });
-    const player = createAudioPlayer()
-    var resource = createAudioResource(stream)
-    player.play(resource)
-    connection.subscribe(player);
-
-    await msg.reply(`:arrow_forward:  Now Playing *** ${song.title} ***`)
+    } catch(err) {
+        await msg.reply(`:arrow_forward: Wrong link`)
+    }
 }
